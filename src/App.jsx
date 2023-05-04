34ba2e38b0c81fcbf3bc6e1be5d955f84e24422f
Elyas Rodrigues <elyasrz11@gmail.com>
2023-05-04 14:18:10 -0300

    foudasse

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>REACT ROUTER</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// 1 - CONFIGURAÇÃO ROUTER
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+
+import Home from './routes/Home'
+import Contact from './routes/Contact'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: 'contact',
+    element: <Contact/>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
